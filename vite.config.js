a69437537a0f4179325124034af819159acb3e06
Cody Barr <cody.barr@gmail.com>
2022-03-23 09:59:20 -0400

    initial commit 🥃

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'MtG Life Counter',
+        short_name: 'Life Counter',
+        description:
+          'Simple PWA for tracking magic the gathering life / damage.',
+        theme_color: '#FFFFFF',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+    react(),
+  ],
+})
